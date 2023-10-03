57581f998d34aca0ca0afa410b52ed44ce0de123
Uche Ofia <coolucheofia@gmail.com>
2023-10-03 04:00:57 +0100

    fix: Update storage models

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,41 +27,35 @@ classes = {
 
 
 class DBStorage:
-    """Interacts with the MySQL database"""
+    """interaacts with the MySQL database"""
 
     __engine = None
     __session = None
 
     def __init__(self):
         """Instantiate a DBStorage object"""
-        hbnb_user = getenv("HBNB_MYSQL_USER")
-        hbnb_pwd = getenv("HBNB_MYSQL_PWD")
-        hbnb_host = getenv("HBNB_MYSQL_HOST")
-        hbnb_db = getenv("HBNB_MYSQL_DB")
-        hbnb_env = getenv("HBNB_ENV")
-        url = "mysql+mysqldb://{}:{}@{}/{}".format(
-            hbnb_user, hbnb_pwd, hbnb_host, hbnb_db
+        HBNB_MYSQL_USER = getenv("HBNB_MYSQL_USER")
+        HBNB_MYSQL_PWD = getenv("HBNB_MYSQL_PWD")
+        HBNB_MYSQL_HOST = getenv("HBNB_MYSQL_HOST")
+        HBNB_MYSQL_DB = getenv("HBNB_MYSQL_DB")
+        HBNB_ENV = getenv("HBNB_ENV")
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@{}/{}".format(
+                HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB
+            )
         )
-        self.__engine = create_engine(url, pool_pre_ping=True)
-
-        if hbnb_env == "test":
+        if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """query on the current database session"""
         new_dict = {}
-        if cls:
-            # If a class is provided, query objects only for that class.
-            objs = self.__session.query(cls).all()
-        else:
-            # If cls is not provided, query objects for all registered classes.
-            objs = []
-            for clss in classes.values():
-                objs += self.__session.query(clss).all()
-
-        for obj in objs:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            new_dict[key] = obj
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + "." + obj.id
+                    new_dict[key] = obj
         return new_dict
 
     def new(self, obj):
@@ -81,20 +75,21 @@ class DBStorage:
         """reloads data from the database"""
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        session = scoped_session(sess_factory)
-        self.__session = session
-
-    def get(self, cls, id):
-        """Retrieve one object"""
-        if cls is None or id is None:
-            return None
-        objs = self.__session.query(cls).all()
-        return next((obj for obj in objs if obj.id == id), None)
-
-    def count(self, cls=None):
-        """Count the number of objects in storage"""
-        return len(self.all(cls))
+        Session = scoped_session(sess_factory)
+        self.__session = Session
 
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve object based on the class and"""
+        if cls and id:
+            return self.__session.query(cls).get(id)
+        return None
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        if cls:
+            return self.__session.query(cls).count()
+        return sum(self.__session.query(c).count() for c in classes.values())
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,24 +35,24 @@ class FileStorage:
     def all(self, cls=None):
         """returns the dictionary __objects"""
         if cls is not None:
-            return {
-                key: value
-                for key, value in self.__objects.items()
-                if cls in [value.__class__, value.__class__.__name__]
-            }
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
         return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        json_objects = {
-            key: self.__objects[key].to_dict() for key in self.__objects
-        }
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
         with open(self.__file_path, "w") as f:
             json.dump(json_objects, f)
 
@@ -63,27 +63,36 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except Exception:
+        except:
             pass
 
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
+
+    def close(self):
+        """
+        call reload() method for
+        deserializing the JSON file to objects
+        """
+        self.reload()
+
     def get(self, cls, id):
-        """retrieves one object"""
-        if cls is not None and id is not None:
+        """Retrieve one object based on the class and its ID."""
+        if cls and id:
             key = "{}.{}".format(cls.__name__, id)
             return self.__objects.get(key)
         return None
 
     def count(self, cls=None):
-        """count the number of objects in storage"""
-        return len(self.all(cls)) if cls is not None else len(self.all())
-
-    def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
-        if obj is not None:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            if key in self.__objects:
-                del self.__objects[key]
-
-    def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
-        self.reload()
+        """Count the number of objects in storage."""
+        if cls:
+            return sum(
+                1
+                for key, value in self.__objects.items()
+                if value.__class__ == cls or value.__class__.__name__ == cls
+            )
+        return len(self.__objects)
